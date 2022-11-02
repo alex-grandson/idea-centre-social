a10b116441234a1aacb556d47323e1f5a68f5e5a
x1kk4 <97203947+x1kk4@users.noreply.github.com>
2022-11-03 01:54:28 +0300

    General layout + projects page  (#1)
    * Project_card
    
    * projs_content
    
    * header buttons state and alignment
    
    * dataset + types
    
    * neploho

---
+++ src/components/global/ContentContainer.tsx
@@ -0,0 +1,19 @@
+import react, { Children, FC, useEffect } from 'react'
+import Box from '@mui/material/Box'
+import Grid from '@mui/material/Grid'
+
+interface ContentContainerProps {
+  children: React.ReactNode
+}
+
+const ContentContainer: FC<ContentContainerProps> = ({ children }) => {
+  return (
+    <Box sx={{ width: '70%', m: '0 auto' }}>
+      <Grid container direction='row'>
+        {children}
+      </Grid>
+    </Box>
+  )
+}
+
+export default ContentContainer
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import Head from 'next/head'
 import theme from '../theme'
 import { ThemeProvider } from '@mui/material/styles'
+import Header from '../components/global/Header'
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache()
@@ -24,9 +25,11 @@ export default function MyApp(props: MyAppProps) {
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <ThemeProvider theme={theme}>
+        <Header>
+          <Component {...pageProps} />
+        </Header>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
   )
