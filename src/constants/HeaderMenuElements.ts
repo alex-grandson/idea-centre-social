1ddf61a1d951bdcb5481a7469f1ae00069b75c21
x1kk4 <grigory.valevin@gmail.com>
2022-11-02 19:28:26 +0300

    header buttons state and alignment

---
+++ src/constants/HeaderMenuElements.ts
@@ -0,0 +1,15 @@
+import { HeaderMenuType } from '../types/HeaderMenuType'
+export const headerMenuElements: HeaderMenuType[] = [
+  {
+    title: 'Идеи',
+    url: '/projects',
+  },
+  {
+    title: 'Анкеты',
+    url: '/profiles',
+  },
+  {
+    title: 'Сообщения',
+    url: '/chat',
+  },
+]
