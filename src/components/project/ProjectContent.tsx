a60685b99cf7fe0348d048371fea264a18405d08
x1kk4 <grigory.valevin@gmail.com>
2022-11-03 03:08:36 +0300

    add modal content to project cards

--- src/components/project/ProjectContent.tsx
+++ src/components/project/ProjectContent.tsx
@@ -6,6 +6,12 @@ import Avatar from '@mui/material/Avatar'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import CardMedia from '@mui/material/CardMedia'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import CommentIcon from '@mui/icons-material/Comment'
+import IconButton from '@mui/material/IconButton'
+import { Project } from '../../types/Project'
 
 const style = {
   display: 'inline-block',
@@ -20,7 +26,11 @@ const style = {
   p: '0 0 32px 0',
 }
 
-const ProjectContent: FC = () => {
+interface ProjectCardProps {
+  project: Project
+}
+
+const ProjectContent: FC<ProjectCardProps> = ({ project }) => {
   return (
     <Card
       sx={{
@@ -38,15 +48,15 @@ const ProjectContent: FC = () => {
       <CardMedia
         component='img'
         height='350px'
-        image='https://sun9-4.userapi.com/impg/koPgnUiEOQWr95sZu3M7EBTMDWdmHR7E3aGAWA/rh6-eoWGwyU.jpg?size=1262x1000&quality=95&sign=bda36a2361801f2c3d2e699e378e3edd&type=album'
-        alt='iguana (perico)'
+        image={project.image}
+        alt={project.name}
       />
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
-          01.11.2022
+          {project.date}
         </Typography>
         <Typography variant='h5' component='div'>
-          Уборка улиц
+          {project.name}
         </Typography>
         <Box
           sx={{
@@ -56,22 +66,23 @@ const ProjectContent: FC = () => {
             height: '50px',
           }}
         >
-          <Avatar
-            alt='Леха внук'
-            src='https://sun9-80.userapi.com/impg/kmXHp2VTZ8poy1PUH1Pnea3s6nFf188nKzjYLw/WkQPl1rWALI.jpg?size=1266x1688&quality=95&sign=8a83cb2e7a33ef7636c2510c3e30e604&type=album'
-          />
-          <Typography sx={{ alignSelf: 'center' }}>Леха внук</Typography>
+          <Avatar alt={project.creator.firstname} src={project.creator.image} />
+          <Typography
+            sx={{ alignSelf: 'center' }}
+          >{`${project.creator.firstname} ${project.creator.lastname}`}</Typography>
         </Box>
-        <Typography variant='body2' sx={{ mt: '10px' }}>
-          Требуется:
-          <br />
-          - Дворник
-          <br />
-          - Специалист по лазерам
-          <br />
-          - Фронтендер
-          <br />- Танкист
-        </Typography>
+        {project.slots.map((value) => (
+          <ListItem
+            disableGutters
+            secondaryAction={
+              <IconButton aria-label='comment'>
+                <CommentIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary={`${value.category}`} />
+          </ListItem>
+        ))}
       </CardContent>
     </Card>
   )
